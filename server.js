9df230bc5bc93e9ffb461898b54fc996dcef905a
JKKook <whdrnrdldia@naver.com>
2023-04-18 23:40:00 +0900

    DOC

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 // 서버를 띄우기 위한 기본 셋팅
-const express = require('express'); // 라이    브러리 사용
-const app = express(); // 라이브러리를 사용해서 서버 생성
-// 생성한 서버를 지정 포트에 열어 줌
+const express = require('express');
+const app = express();
 app.listen(8080, () => {
-    console.log('listening on 8080'); // 지정 포트를 열면 보여 줄 부분
+    console.log('listening on 8080');
 });
 
 app.get('/', (req, res) => {
